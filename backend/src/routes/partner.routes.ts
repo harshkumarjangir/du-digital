942b0a5c920f4cf74f7db82fb7f2619b9e91b7db
harshkumarjangir <harshkumarjangir2002@gmail.com>
2026-01-03 17:13:22 +0530

    oo

--- backend/src/routes/partner.routes.ts
+++ backend/src/routes/partner.routes.ts
@@ -8,9 +8,9 @@ const router = express.Router();
 
 
 router.post("/", createPartnerRequest);
+router.get("/official", getOfficialPartners);
 router.use(protect);
 router.get("/", getPartnerRequests);
-router.get("/official", getOfficialPartners);
 router.put("/:id", hasPermission("manage_partners"), upload.fields([{ name: 'logo', maxCount: 1 }, { name: 'userImage', maxCount: 1 }]), updatePartnerStatus);
 router.get("/stats", getPartnerStats);
 
