0b269122b9f7798e0c4026e9c24be7102996d4e7
Shivam Swami <123632262+dev-Shivam999@users.noreply.github.com>
2026-03-13 17:55:13 +0530

    code updated

--- backend/src/utils/sms.ts
+++ backend/src/utils/sms.ts
@@ -1,19 +1,32 @@
 import axios from "axios";
 
-export async function sendOtp(mobile: string, otp: string) {
-    const url = `https://mobicomm.dove-sms.com//submitsms.jsp?user=DUDigi&key=b70b24ab5fXX&mobile=${String(mobile.replace("+91",""))}&message=${otp} is your verification code for your enquiry with DU Global. Valid for 10 minutes. Please do not share this OTP. Team DUDigital Global Ltd&senderid=INFOSM&accusage=1&entityid=1234567891112131415&tempid=1034567891112131819`;
+export async function sendOtp(mobile:string, otp:string) {
+  const url = `https://mobicomm.dove-sms.com//REST/sendsms/`;
 
+  try {
+    const response = await axios.post(url, {
+      listsms: [
+        {
+          sms: `${otp} is your verification code for your enquiry with DU Global. Valid for 10 minutes. Please do not share this OTP. Team DUDigital Global Ltd`,
+          mobiles: mobile,
+          senderid: "DIGIBP",
+          clientsmsid: "DUDigi",
+          accountusagetypeid: "1",
+          entityid: "1701163670907797721",
+          tempid: "1707177028462766869",
+        },
+      ],
+      password: "b70b24ab5fXX",
+      user: "DUDigi",
+    });
   
-    try {
-        const response = await axios.get(url);
-    if(!response.data.includes("InvalidMobileNumber")){
-        return true;
-    }else{
-        return false;
+    if (response.data.smslist.sms.status=="success") {
+      return true;
+    } else {
+      return false;
     }
-    } catch (error: any) {
-        console.error("SMS Failed:", error.message);
-        return false;
-    }
+  } catch (error:any) {
+    console.error("SMS Failed:", error.message);
+    return false;
+  }
 }
-
